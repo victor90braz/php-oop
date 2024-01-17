8bf511c579d6ef5f6184dc8073a848d057d53a6d
victor braz <victor.90braz@gmail.com>
2024-01-17 15:36:30 +0100

    update url strings

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/app.css">
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+    <article>
+        <?= $post; ?>
+    </article>
+
+    <span>
+        <a href="/">Back</a>
+    </span>
+</body>
+</html>
