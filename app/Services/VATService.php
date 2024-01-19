1b7199ea5c8a892a286f166e3e551669aea6effc
victor braz <victor.90braz@gmail.com>
2024-01-19 12:23:59 +0100

    calculateVatAmount from the request

--- app/Services/VATService.php
+++ app/Services/VATService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
-class VATService extends AbstractService
+use Illuminate\Support\Facades\Http;
+
+class VATService
 {
     /**
      * @param float $price
@@ -11,7 +13,26 @@ class VATService extends AbstractService
     public function calculateVatAmount(float $price): float
     {
         $response = $this->callExternalAPI($price);
-
         return $response->json('vat_amount');
     }
+
+    /**
+     * @param float $price
+     * @return \Illuminate\Http\Client\Response
+     */
+    protected function callExternalAPI(float $price)
+    {
+
+        $apiUrl = 'https://vat.abstractapi.com/v1/calculate';
+        $apiKey = '2d1cf30f3e364b17ac99d68a4011aea2';
+        $countryCode = 'DE';
+
+        return Http::get($apiUrl, [
+            'api_key' => $apiKey,
+            'amount' => $price,
+            'country_code' => $countryCode
+        ]);
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
-    return view('posts', [
-        'posts' => \App\Models\Post::all()
-    ]);
+    return view('welcome');
 });
 
-Route::get('posts/{post}', function ($slug) {
-
-    return view('post', [
-        'post' => \App\Models\Post::find($slug)
-    ]);
-
-})->where('post', '[A-z_\-]+');
+Route::get('/vatservice/', function () {
+    return (new \App\Services\VATService())->calculateVatAmount(request('amount'));
+});
 
